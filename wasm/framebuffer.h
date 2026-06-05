23fed3232447107ce2dcd60bb374f8ca48aae186
Iain Donald <iaindonald@outlook.com>
2026-06-04 19:28:26 -0700

    Optimize framebuffer.h

--- wasm/framebuffer.h
+++ wasm/framebuffer.h
@@ -7,52 +7,41 @@
 
 // Colors: 1 byte per channel, 4 bytes per pixel.
 // 0xAABBGGRR
-#define BLACK  0xFF000000
-#define WHITE  0xFFFFFFFF
-#define RED    0xFF0000FF
-#define GREEN  0xFF00FF00
-#define BLUE   0xFFFF0000
 
 // state
 static uint32_t fbPrev[FB_MAX_PIXELS]; // Stores previous frame to avoid full redraw. Pending complete implementation.
 static uint32_t fb[FB_MAX_PIXELS];
-static int fb_w    = 640;
-static int fb_h    = 480;
-static int mouse_x = -1;
-static int mouse_y = -1;
+static uint16_t fb_w = 640;
+static uint16_t fb_h = 480;
+static uint16_t mouse_x = -1;
+static uint16_t mouse_y = -1;
 
 // exports
 EXPORT uint32_t* get_fb() { return fb; }
-EXPORT int       get_w()  { return fb_w; }
-EXPORT int       get_h()  { return fb_h; }
-
-EXPORT void set_mouse(int x, int y) { mouse_x = x; mouse_y = y; }
-
-EXPORT void set_size(int w, int h) {
+EXPORT uint16_t get_w() { return fb_w; }
+EXPORT uint16_t get_h() { return fb_h; }
+EXPORT void set_mouse(uint16_t x, uint16_t y) { mouse_x = x; mouse_y = y; }
+EXPORT void set_size(uint16_t w, uint16_t h) {
     if (w * h > FB_MAX_PIXELS) return;
     fb_w = w; fb_h = h;
 }
 
 // drawPixel: unchecked. Clip upstream or use rect().
-static inline void drawPixel(int x, int y, uint32_t color) {
+static inline void drawPixel(uint16_t x, uint16_t y, uint32_t color) {
     fb[y * fb_w + x] = color;
 }
 
-static void clear(uint32_t color) {
-    for (int i = 0; i < fb_w * fb_h; i++)
-        fb[i] = color;
+static void clear() {
+    for (uint32_t i = 0; i < fb_w * fb_h; i++)
+        *(fb + i) = 0x00000000;
 }
 
-static void rect(int x, int y, int w, int h, uint32_t color) {
-    // clip to framebuffer bounds
-    int x0 = x < 0      ? 0    : x;
-    int y0 = y < 0      ? 0    : y;
-    int x1 = x+w > fb_w ? fb_w : x+w;
-    int y1 = y+h > fb_h ? fb_h : y+h;
-    // draw
-    for (int py = y0; py < y1; py++)
-        for (int px = x0; px < x1; px++)
-            drawPixel(px, py, color);
+static void rect(uint16_t x, uint16_t y, uint16_t w, uint16_t h, uint32_t color) {
+    for (uint16_t py = y; py < y + h; py++) {
+        uint32_t *dst = &fb[py * fb_w + x];
+        for (uint16_t i = 0; i < w; i++)
+            dst[i] = color;
+    }
 }
 
 EXPORT void render_frame();
--- wasm/testing.c
+++ wasm/testing.c
@@ -5,7 +5,7 @@ static int color_toggle = 0;
 void on_click()      { color_toggle = !color_toggle; }
 
 void render_frame() {
-    clear(BLACK);
+    clear();
     if (mouse_x >= 0 && mouse_y >= 0)
         rect(mouse_x, mouse_y, 10, 10, color_toggle ? GREEN : WHITE);
 }
@@ -13,4 +13,5 @@ void render_frame() {
 void render_blocks() {
     // for drawing, calculate difference between changed pixels before and after. 
     // Pixel p, frame n. pn-2, pn-1, and pn. 
+    // Draw pixel:  
 }
